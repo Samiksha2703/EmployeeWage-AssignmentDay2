52fdef0036ce0bbdac58e5af3f8a2760096438a5
samiksha Shende <shende.samiksha@rediffmail.com>
2020-12-01 09:53:34 +0530

    Add Daily Employee Wage Code

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -11,3 +11,28 @@ else {
     console.log("Employee is PRESENT.");
 }
 }
+
+// UC2-DailyEmployeeWage
+
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOURS = 20;
+
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+console.log(empCheck);
+switch (empCheck){
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HOURS;
+        break;
+    default:
+        empHrs = 0;
+}
+
+let empWage = empHrs * WAGE_PER_HOURS;
+console.log("Employee Wage : "+empWage);
